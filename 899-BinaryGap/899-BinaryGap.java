1c76fd85b4192d9d9d0749c45c8ecc81023a8812
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:03:28 +0530

    Create 899-BinaryGap

---
+++ 899-BinaryGap/899-BinaryGap.java
@@ -0,0 +1,18 @@
+// Last updated: 7/8/2026, 5:03:28 PM
+class Solution {
+    public int binaryGap(int n) {
+        char[] c = Integer.toBinaryString(n).toCharArray();
+        int max = 0;
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '1') {
+                for (int j = i + 1; j < c.length; j++) {
+                    if (c[j] == '1') {
+                        max=Math.max(max,j-i);
+                        break;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
