c81462d608fdbf525f129dc64f807bd490acb5e2
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:05:00 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,24 @@
+// Last updated: 7/8/2026, 5:05:00 PM
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> st = new HashSet<>();
+        while (!st.contains(n)) {
+            st.add(n);
+            n = check(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int check(int n) {
+        int output = 0;
+        while (n > 0) {
+            int r = n % 10;
+            output += r * r;
+            n = n / 10;
+        }
+        return output;
+    }
+}
