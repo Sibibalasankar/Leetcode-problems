edb8203c368c3bdc5eb2feed7628b511a7f1571e
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:04:14 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.java
@@ -0,0 +1,17 @@
+// Last updated: 7/8/2026, 5:04:13 PM
+class Solution {
+    public String reverseWords(String s) {
+        String[] s1 = s.split(" ");
+        String result = "";
+
+        for (int i = 0; i < s1.length; i++) {
+            StringBuilder str = new StringBuilder(s1[i]);
+            if (s1.length-1 != i) {
+                result += str.reverse().toString() + " ";
+            } else {
+                result += str.reverse().toString();
+            }
+        }
+        return result;
+    }
+}
