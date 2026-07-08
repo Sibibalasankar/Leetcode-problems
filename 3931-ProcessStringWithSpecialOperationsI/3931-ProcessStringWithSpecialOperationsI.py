ef79ba02bb62a1bc52444e35ef10a2a8482361ab
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 16:59:58 +0530

    Create 3931-ProcessStringWithSpecialOperationsI

---
+++ 3931-ProcessStringWithSpecialOperationsI/3931-ProcessStringWithSpecialOperationsI.py
@@ -0,0 +1,21 @@
+# Last updated: 7/8/2026, 4:59:58 PM
+#translated using AI
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = []
+        n = len(s)
+
+        for i in range(n):
+            ch = s[i]
+
+            if ch == '*':
+                if len(res) != 0:
+                    res.pop()
+            elif ch == '#':
+                res.extend(res)
+            elif ch == '%':
+                res.reverse()
+            elif 'a' <= ch <= 'z':
+                res.append(ch)
+
+        return ''.join(res)
