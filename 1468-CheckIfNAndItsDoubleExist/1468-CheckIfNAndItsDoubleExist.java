a19442366fe609c1b506cb24981d5c9b0410ba87
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:02:54 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,13 @@
+// Last updated: 7/8/2026, 5:02:54 PM
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
