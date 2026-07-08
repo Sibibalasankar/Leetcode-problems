c20d71f8a22f9bc2152224dac78bbb76e58fd240
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:02:53 +0530

    Create 1472-IncreasingDecreasingString

---
+++ 1472-IncreasingDecreasingString/1472-IncreasingDecreasingString.java
@@ -0,0 +1,27 @@
+// Last updated: 7/8/2026, 5:02:52 PM
+class Solution {
+    public String sortString(String s) {
+        StringBuilder result = new StringBuilder();
+        int[] freqArray = new int[26];
+        for(char ch : s.toCharArray()) 
+            freqArray[ch - 'a']++;
+
+        while(result.length() < s.length()) {
+            for(int index = 0; index < 26; index++) {
+                if(freqArray[index] > 0) {
+                    result.append((char) (index + 'a'));
+                    freqArray[index] -= 1;
+                }
+            }
+
+            for(int index = 25; index >= 0; index--) {
+                if(freqArray[index] > 0) {
+                    result.append((char) (index + 'a'));
+                    freqArray[index] -= 1;
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
