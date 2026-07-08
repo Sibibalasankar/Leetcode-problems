2cc00f645e360cc93c062e062bac76476c2e6652
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:02:29 +0530

    Create 1756-MinimumDeletionsToMakeStringBalanced

---
+++ 1756-MinimumDeletionsToMakeStringBalanced/1756-MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,20 @@
+// Last updated: 7/8/2026, 5:02:28 PM
+class Solution {
+    public int minimumDeletions(String s) {
+        int res = 0;
+        int count = 0; // Tracks the number of 'b's seen so far
+        
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                count++;
+            } else if (count != 0) {
+                // Found an 'a' after 'b's.
+                // We need to delete one character to resolve this "ba" pair.
+                res++;
+                count--;
+            }
+        }
+        
+        return res;
+    }
+}
