ebede269dbfad0219854717a50713ba796595201
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:04:36 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,18 @@
+// Last updated: 7/8/2026, 5:04:36 PM
+import java.util.*;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+}
