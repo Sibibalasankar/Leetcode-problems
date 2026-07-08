7b8cd41edce91d55a4eccfa98bcfff73c4627f39
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:01:21 +0530

    Create 2550-WordsWithinTwoEditsOfDictionary

---
+++ 2550-WordsWithinTwoEditsOfDictionary/2550-WordsWithinTwoEditsOfDictionary.java
@@ -0,0 +1,34 @@
+// Last updated: 7/8/2026, 5:01:21 PM
+class Solution {
+    boolean hasMatch(String query, String[] dictionary) {
+        boolean found = false;
+        for (String dict : dictionary) {
+            int diffMax = 2;
+            for (int i=0;i<dict.length();i++) {
+                if (dict.charAt(i) != query.charAt(i)) {
+                    diffMax--;
+                }
+                if (diffMax < 0) {
+                    break;
+                }
+            }
+            if (diffMax >= 0) {
+                return true;
+            }
+        }
+
+        return found;
+    }
+
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+
+        List<String> result = new ArrayList<>();
+        for (String query : queries) {
+            if (hasMatch(query, dictionary)) {
+                result.add(query);
+            }
+        }
+
+        return result;
+    }
+}
