9ed11658760240f996ed539c1f70938cb9372b86
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:00:59 +0530

    Create 2892-CheckIfArrayIsGood

---
+++ 2892-CheckIfArrayIsGood/2892-CheckIfArrayIsGood.java
@@ -0,0 +1,22 @@
+// Last updated: 7/8/2026, 5:00:58 PM
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length - 1;
+        Set<Integer> seen = new HashSet<>();
+        boolean dup = false;
+
+        for (int num : nums) {
+            if (num > n) return false;
+
+            if (seen.contains(num)) {
+                if (num < n || dup) return false;
+                dup = true;
+                continue;
+            }
+
+            seen.add(num);
+        }
+
+        return true;
+    }
+}
