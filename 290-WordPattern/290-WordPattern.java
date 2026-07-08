1e1a55e80f987b9bdc0db58ce3fb160dca086d87
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:04:41 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,28 @@
+// Last updated: 7/8/2026, 5:04:40 PM
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+        if (pattern.length() != arr.length) {
+            return false;
+        }
+        HashSet<String> set = new HashSet<>();
+        HashMap<Character, String> map = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = arr[i];
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(w)) {
+                    return false;
+                }
+            } else {
+                if (set.contains(w)) {
+                    return false;
+                }
+                set.add(w);
+                map.put(c, w);
+            }
+
+        }
+        return true;
+    }
+}
