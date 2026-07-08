bb719b452f2a24e34333ff23fff44bee103b6f38
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:05:59 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,6 @@
+# Last updated: 7/8/2026, 5:05:58 PM
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+      for j in range(n):
+          nums1[m+j] = nums2[j]
+      nums1.sort()
