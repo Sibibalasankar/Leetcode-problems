3a8003fc0021f95bae0a46689508e94a06e99878
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 16:59:00 +0530

    Create 4139-MinimumAbsoluteDistanceBetweenMirrorPairs

---
+++ 4139-MinimumAbsoluteDistanceBetweenMirrorPairs/4139-MinimumAbsoluteDistanceBetweenMirrorPairs.java
@@ -0,0 +1,25 @@
+// Last updated: 7/8/2026, 4:58:59 PM
+class Solution {
+    private int reverse(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+
+    public int minMirrorPairDistance(int[] nums) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        int n = nums.length, ans = (int) 1e6;
+
+        for (int i = 0; i < n; i++) {
+            if (mpp.containsKey(nums[i])) {
+                ans = Math.min(ans, i - mpp.get(nums[i]));
+            }
+            mpp.put(reverse(nums[i]), i);
+        }
+
+        return ans == (int) 1e6 ? -1 : ans;
+    }
+}
