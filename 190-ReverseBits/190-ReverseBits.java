32a32f8cb6a45fcef760847344e6e11193c1a53f
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:05:05 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.java
@@ -0,0 +1,15 @@
+// Last updated: 7/8/2026, 5:05:05 PM
+class Solution {
+    public int reverseBits(int n) { return rev(n, 32); }
+
+    private int rev(int v, int len) {
+        if (len == 1) return v & 1;
+
+        int half = len >> 1;
+        int mask = (1 << half) - 1;
+        int lo = v & mask;
+        int hi = v >>> half;
+
+        return (rev(lo, half) << half) | rev(hi, half);
+    }
+}
