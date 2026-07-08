b0f951bafd6f3e4338fb011383787345fccaae44
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:00:42 +0530

    Create 3263-DivideAnArrayIntoSubarraysWithMinimumCostI

---
+++ 3263-DivideAnArrayIntoSubarraysWithMinimumCostI/3263-DivideAnArrayIntoSubarraysWithMinimumCostI.java
@@ -0,0 +1,18 @@
+// Last updated: 7/8/2026, 5:00:42 PM
+class Solution {
+    public int minimumCost(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+        
+        return nums[0] + min1 + min2;
+    }
+}
