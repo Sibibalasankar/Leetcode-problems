469aef9aae71c4e9e1d1477dbd6ecece833a3369
Sibi <154968736+Sibibalasankar@users.noreply.github.com>
2026-07-08 17:02:23 +0530

    Create 1800-ConcatenationOfConsecutiveBinaryNumbers

---
+++ 1800-ConcatenationOfConsecutiveBinaryNumbers/1800-ConcatenationOfConsecutiveBinaryNumbers.java
@@ -0,0 +1,15 @@
+// Last updated: 7/8/2026, 5:02:23 PM
+class Solution {
+    int mod = 1000000007;
+
+    public int concatenatedBinary(int n) {
+        int len = 0;
+        long res = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0)len++;
+            res = ((res << len) + i) % mod;
+        }
+        return (int) res;
+    }
+}
